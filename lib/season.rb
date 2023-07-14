efe33800bbf2fdfa51ea0497d24b107f0f918c8e
Jennifer Rush <jennifer.rush2@gmail.com>
2023-07-14 14:49:13 -0600

    Check that game season and Season match before adding game to a Season

--- lib/season.rb
+++ lib/season.rb
@@ -10,6 +10,12 @@ class Season
     end
 
     def add_game(game_to_add)
-        @games << game_to_add
+        if game_to_add.season == season_years
+            @games << game_to_add
+        else
+            p "Error: that game is from a different season. Please try again"
+            @games 
+        end
+        @games 
     end
 end
